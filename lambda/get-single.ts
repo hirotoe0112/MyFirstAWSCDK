2887a0f589c0999fcbe455d27edf0ddc1f2472f0
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2022-06-19 09:42:26 +0900

    getsingleの実装

--- lambda/get-single.ts
+++ lambda/get-single.ts
@@ -4,13 +4,27 @@ const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME=process.env.TABLE_NAME || "";
 
 export const handler = async (event: any): Promise<any> => {
-  const requestBody = event;
+  const requestBody = event.pathParameters;
+  const userId = requestBody.userId;
+  const taskId = requestBody.taskId;
+  const params = {
+    TableName:TABLE_NAME,
+    Key:{
+      userId:userId,
+      taskId:taskId
+    }
+  }
 
-  return { 
-    statusCode: 200,
-    headers: {
-      'Content-Type':'application/json'
-    },
-    body: JSON.stringify(requestBody)
-  };
+  try{
+    const data = await db.get(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  }catch(e){
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e)
+    };
+  }
 }
