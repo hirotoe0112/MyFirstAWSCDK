40cad02ccdb186761f5d502a46a6054435606f05
hirotoe0112 <hirotoe0112@gmail.com>
2022-06-19 11:40:52 +0900

    deleteを実装

--- lambda/get-all.ts
+++ lambda/get-all.ts
@@ -3,9 +3,10 @@ import * as AWS from 'aws-sdk';
 const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME=process.env.TABLE_NAME || "";
 
-export const handler = async (event: any): Promise<any> => {
+export const handler = async (event: any,): Promise<any> => {
   const requestBody = event.pathParameters;
   const userId = requestBody.userId;
+
   const params = {
     TableName:TABLE_NAME,
     KeyConditionExpression: 'userId = :userId',
--- lambda/get-single.ts
+++ lambda/get-single.ts
@@ -3,10 +3,11 @@ import * as AWS from 'aws-sdk';
 const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME=process.env.TABLE_NAME || "";
 
-export const handler = async (event: any): Promise<any> => {
+export const handler = async (event: any,): Promise<any> => {
   const requestBody = event.pathParameters;
   const userId = requestBody.userId;
   const taskId = requestBody.taskId;
+
   const params = {
     TableName:TABLE_NAME,
     Key:{
