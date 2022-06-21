ac548207b2af92329297cf2eccc43c15d88b293c
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2022-06-21 15:51:35 +0900

    test code

--- test/todo.test.ts
+++ test/todo.test.ts
@@ -1,10 +1,31 @@
+import * as cdk from 'aws-cdk-lib';
+import * as assert from 'aws-cdk-lib/assertions';
+import { Template } from 'aws-cdk-lib/assertions';
+import { TodoStack } from '../lib/todo-stack';
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as CdkDemo from '../lib/cdk-demo-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-demo-stack.ts
-test('SQS Queue Created', () => {
+test('fine grained assertions', () => {
+    const app = new cdk.App(
+        {
+            context:{
+                "variables":{
+                    "target":"dev"
+                }
+            }
+        }
+    )
+
+
+    const todoStack = new TodoStack(app, 'test-todo');
+
+    const template = Template.fromStack(todoStack);
+
+    template.resourceCountIs('AWS::Lambda::Function', 5);
+
 //   const app = new cdk.App();
 //     // WHEN
 //   const stack = new CdkDemo.CdkDemoStack(app, 'MyTestStack');
