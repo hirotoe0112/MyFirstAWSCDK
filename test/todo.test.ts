9174abeb0bae1e53037f4fb5db5e57786a6f828f
hirotoe0112 <hirotoe0112@gmail.com>
2022-06-25 22:38:02 +0900

    リソース数のテスト

--- test/todo.test.ts
+++ test/todo.test.ts
@@ -8,75 +8,68 @@ import { TodoStack } from '../lib/todo-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-demo-stack.ts
-test('Snapshot test', () => {
-    /**
-     * Set up
-     */
-    const app = new cdk.App(
-        {
-            context:{
-                "variables":{
-                    "target":"dev"
-                }
-            }
+test('Resources counts', () => {
+  /**
+   * Set up
+   */
+  const app = new cdk.App(
+    {
+      context: {
+        "variables": {
+          "target": "dev"
         }
-    )
-    const todoStack = new TodoStack(app, 'test-todo');
-    const template = Template.fromStack(todoStack);
+      }
+    }
+  )
+  const todoStack = new TodoStack(app, 'test-todo');
+  const template = Template.fromStack(todoStack);
 
-    /**
-     * Test
-     */
-    //expect(template.toJSON()).toMatchSnapshot();
+  /**
+   * Test
+   */
+  template.resourceCountIs('AWS::DynamoDB::Table', 1);
+  template.resourceCountIs('AWS::Lambda::Function', 8);
+  template.resourceCountIs('AWS::ApiGateway::RestApi', 1);
+  template.resourceCountIs('AWS::Cognito::UserPool', 1);
+  template.resourceCountIs('AWS::Cognito::UserPoolClient', 1);
+  template.resourceCountIs('AWS::ApiGateway::Authorizer', 1);
+  template.resourceCountIs('AWS::ApiGateway::Model', 4);
+  template.resourceCountIs('AWS::ApiGateway::RequestValidator', 4);
 })
 
-test('Fine-grained assertions', () => {
-    /**
-     * Set up
-     */
-    const app = new cdk.App(
-        {
-            context:{
-                "variables":{
-                    "target":"dev"
-                }
-            }
+test('apigateway settings', () => {
+  /**
+   * Set up
+   */
+  const app = new cdk.App(
+    {
+      context: {
+        "variables": {
+          "target": "dev"
         }
-    )
-    const todoStack = new TodoStack(app, 'test-todo');
-    const template = Template.fromStack(todoStack);
+      }
+    }
+  )
+  const todoStack = new TodoStack(app, 'test-todo');
+  const template = Template.fromStack(todoStack);
 
-    /**
-     * Test
-     */
-    template.resourceCountIs('AWS::Lambda::Function', 8);
-    template.resourceCountIs('AWS::DynamoDB::Table', 1);
-
-    template.hasResource('AWS::ApiGateway::RestApi', {});
-    template.hasResourceProperties('AWS::ApiGateway::Resource', {
-        PathPart: 'tasks',
-    });
-    template.hasResourceProperties('AWS::ApiGateway::Resource', {
-        PathPart: '{taskId}',
-    });
-    template.hasResourceProperties('AWS::ApiGateway::Resource', {
-        PathPart: 'users',
-    });
-    template.hasResourceProperties('AWS::ApiGateway::Resource', {
-        PathPart: 'auth',
-    });
-    template.hasResourceProperties('AWS::ApiGateway::Resource', {
-        PathPart: 'confirm',
-    });
-    template.hasResourceProperties('AWS::ApiGateway::RequestValidator', {});
-
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkDemo.CdkDemoStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  /**
+   * Test
+   */
+  template.hasResourceProperties('AWS::ApiGateway::Resource', {
+    PathPart: 'tasks',
+  });
+  template.hasResourceProperties('AWS::ApiGateway::Resource', {
+    PathPart: '{taskId}',
+  });
+  template.hasResourceProperties('AWS::ApiGateway::Resource', {
+    PathPart: 'users',
+  });
+  template.hasResourceProperties('AWS::ApiGateway::Resource', {
+    PathPart: 'auth',
+  });
+  template.hasResourceProperties('AWS::ApiGateway::Resource', {
+    PathPart: 'confirm',
+  });
+  template.hasResourceProperties('AWS::ApiGateway::RequestValidator', {});
 });
